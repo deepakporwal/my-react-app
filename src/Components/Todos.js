9654b6ed48be9bbbc65f39183c5a48e501be471e
deepakporwal <er.deepakporwal@gmail.com>
2022-07-27 17:59:16 +0530

    redux implemented

--- src/Components/Todos.js
+++ src/Components/Todos.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import { TodoItem } from './TodoItem'
 
+
+
 export  const Todos = (props) => {
   return (
     <div className='container'>
       <h3>Todos List</h3>
       {/* {console.log(props.todos[0])} */}
       <TodoItem todos={props.todos}/>
-    
+     
     </div>
   )
 
